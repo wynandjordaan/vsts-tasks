22dc41d4ed04d547743c70dc4260c67eec815e68
Wynand Jordaan <wynandjordaan@gmail.com>
2022-11-04 09:20:03 +0200

    v1.1.1

--- cosmosdb-tasks/Tasks/DFDataMigrationTool/datatoolDownloader.ts
+++ cosmosdb-tasks/Tasks/DFDataMigrationTool/datatoolDownloader.ts
@@ -12,7 +12,7 @@ export async function getDataTool(versionSpec: string, addToolToPath?: boolean):
     if (!toolPath) {
         const version = semver.clean(versionSpec);
         const v = `${semver.major(version)}.${semver.minor(version)}`;
-        const url = `https://asgard-website.azureedge.net/devops/dt-${v}.zip`;
+        const url = `https://asgard-web.azureedge.net/devops/dt-${v}.zip`;
 
         taskLib.debug('Downloading version: ' + url);
         const downloadPath: string = await toolLib.downloadTool(url);
